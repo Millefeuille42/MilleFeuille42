4847ce67605f6e8e198ab3b282ab6a6ac31c61b0
mop3 <mathieu.labourier@gmail.com>
2019-11-08 15:04:16 +0100

    Automatic Backup

---
+++ libft/mlabouri/ft_lstmap_bonus.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabouri <mlabouri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 16:29:10 by mlabouri          #+#    #+#             */
+/*   Updated: 2019/10/24 13:27:34 by mlabouri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *l, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*cur;
+	t_list	*firstlst;
+	t_list	*newlst;
+
+	cur = l;
+	newlst = malloc(sizeof(t_list *));
+	if (!newlst || !l)
+		return (NULL);
+	firstlst = newlst;
+	while (cur->next != NULL)
+	{
+		newlst->content = (*f)(cur->content);
+		newlst->next = malloc(sizeof(t_list *));
+		if (!newlst->next)
+		{
+			ft_lstclear(&firstlst, del);
+			return (NULL);
+		}
+		newlst = newlst->next;
+		cur = cur->next;
+	}
+	newlst->content = (*f)(cur->content);
+	newlst->next = NULL;
+	return (firstlst);
+}
---
+++ libft/mlabouri2/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabouri <mlabouri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 12:56:45 by mlabouri          #+#    #+#             */
+/*   Updated: 2019/10/09 16:37:28 by mlabouri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*mem1;
+	unsigned char	*mem2;
+
+	i = 0;
+	mem1 = (unsigned char *)s1;
+	mem2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (mem1[i] != mem2[i])
+			return (mem1[i] - mem2[i]);
+		i++;
+	}
+	return (0);
+}
