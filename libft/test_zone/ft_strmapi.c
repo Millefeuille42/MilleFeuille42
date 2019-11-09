5c64236217c20d6d5f302312e0007f5ffd9be477
Microsoft Office Powerpoint 2003 <42064302+mop3@users.noreply.github.com>
2019-11-09 16:21:04 +0100

    Added test_zone for debugging Prepared test templates Seeked potential flaws

---
+++ libft/test_zone/ft_strmapi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabouri <mlabouri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 15:25:22 by mlabouri          #+#    #+#             */
+/*   Updated: 2019/11/05 14:57:21 by mlabouri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//f NULL ?
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	char	*d;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	d = malloc(sizeof(char) * i + 1);
+	if (!d)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		d[i] = (*f)(i, s[i]);
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
---
+++ libft/test_zone/main.c
@@ -0,0 +1,56 @@
+void	ft_test_2str_2fnc(void)
+{
+	printf("ft_atoi\n");
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+}
+
+void	ft_test_2str_1fnc(void)
+{
+	printf("ft_atoi\n");
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+}
+
+void	ft_test_1str_1fnc(void)
+{
+	printf("ft_atoi\n");
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+	printf("LIBFT : %s\n", );
+}
+
+void	ft_test_1str_2fnc(void)
+{
+	printf("ft_atoi\n");
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+}
+
+void	ft_test_int_2fnc(void)
+{
+	printf("ft_atoi\n");
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+	printf("LIBC : %s\nLIBFT : %s\n\n", );
+}
+
+int		main (void)
+{
+
+}
