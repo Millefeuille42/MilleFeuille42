0dd288be58f4137be614be43945e5f62e0fe552a
Microsoft Office Powerpoint 2003 <42064302+mop3@users.noreply.github.com>
2019-11-12 20:11:39 +0100

    Made the function actually work

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mlabouri <mlabouri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 16:14:23 by mlabouri          #+#    #+#             */
-/*   Updated: 2019/11/12 20:04:37 by null             ###   ########.fr       */
+/*   Updated: 2019/11/12 20:11:20 by null             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_gnl_join(size_t rsize, char **buf, char **str)
 	i = 0;
 	while ((*buf)[i] != '\n' && i < rsize)
 		i++;
+	printf("PONG\n");
 	if (!(*str = ft_strjoin_gnl(*str, *buf, i)))
 		return (-1);
 	if ((*buf)[i] == '\n')
@@ -34,6 +35,7 @@ int		ft_gnl_join(size_t rsize, char **buf, char **str)
 		*buf = *buf + i;
 	else
 		*buf = NULL;
+	printf("PONG2\n");
 	return (status);
 }
 
@@ -76,6 +78,7 @@ int		get_next_line(int fd, char **line)
 	printf("first read\n");
 	while (status == 2)
 		status = ft_gnl_join(read(fd, buf, BUFFER_SIZE), &buf, &str);
+	printf("done\n");
 	*line = str;
 	return (status);
 }
