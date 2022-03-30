b52ffa230ebf4c5656a6102b27b13349d5e72da3
Millefeuille42 <mathieu.labourier@gmail.com>
2022-03-30 14:51:13 +0200

    Finished normal_iterators.hpp documentation.

--- In_Progress/ft_containers/iterators/normal_iterators.hpp
+++ In_Progress/ft_containers/iterators/normal_iterators.hpp
@@ -10,42 +10,33 @@
 #include "../utils/exceptions.hpp"
 #include "../utils/type_traits.hpp"
 
-/// Has no interest as is, it's just for reference when building iterators
 namespace ft {
-	template<typename type>
-	class normal_input_iterator : public iterator<std::input_iterator_tag, type> {};
-
-	template<typename type>
-	class normal_output_iterator : public iterator<std::output_iterator_tag, type> {};
-
-	template<typename type>
-	class normal_forward_iterator : public iterator<std::forward_iterator_tag, type> {};
-
-	template<typename type>
-	class normal_bidirectional_iterator : public iterator<std::bidirectional_iterator_tag, type> {};
+	/** @class normal_random_access_iterator
+	 *
+	 * Un wrapper vide sur iterator, permettant de simplifier la syntaxe lorsque l'on construit un random_access_iterator
+	 *
+	 * @inherit Hérite de iterator, avec random_access_iterator_tag comme catégorie
+	 *
+	 * @tparam type < Type sur lequel on souhaite faire pointer l'iterator
+	 */
 
 	template<typename type>
 	class normal_random_access_iterator : public iterator<std::random_access_iterator_tag, type> {};
 
-
-	template <class InputIterator, class Distance>
-	void advance (InputIterator& it, Distance n) {
-		typedef iterator_traits<InputIterator> traits;
-
-		if (is_bidirectional_iterator<typename traits::iterator_category>::value
-			|| is_random_access_iterator<typename traits::iterator_category>::value) {
-			if (n <  0)
-				throw ft::out_of_range();
-		}
-
-		if (is_random_access_iterator<typename traits::iterator_category>::value) {
-			it += n;
-			return;
-		}
-		for (Distance i = 0; i < n; i++) {
-			it++;
-		}
-	}
+	/** @fn trueDistance
+     *
+     * @brief Fonction interne, calculant la distance entre deux iterators
+     *
+     * Fonction interne, calculant la distance entre deux iterators, en allant de first a last,
+     * le random_access_iterator étant le seul a avoir operator -
+     *
+     * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+     * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+     * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+     * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+     * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+     *
+     */
 
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
@@ -57,12 +48,42 @@ namespace ft {
 		return diff;
 	}
 
+	/** @fn trueDistance
+	 *
+	 * @brief Fonction interne, permettant de gérer la distance selon le type d'iterator
+	 *
+	 * Fonction interne, permettant de gérer la distance selon le type d'iterator,
+	 * ici la spécialisation pour les non random_access_iterator et les non output_iterator, utilisant la fonction iterDistance
+	 *
+	 * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+	 * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+	 * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+	 * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+	 * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+     *
+     */
+
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
 	trueDistance(InputIterator first, InputIterator last, std::input_iterator_tag tag) {
 		(void) tag;
 		return iterDistance(first, last);
 	}
+
+	/** @fn trueDistance
+	 *
+	 * @brief Fonction interne, permettant de gérer la distance selon le type d'iterator
+	 *
+	 * Fonction interne, permettant de gérer la distance selon le type d'iterator,
+	 * ici la spécialisation pour les output_iterator, retournant une erreur, la fonction distance ne devant pas fonctionner
+	 *
+	 * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+	 * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+	 * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+	 * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+	 * @return throw une erreur indiquant que l'argument est invalide
+	 *
+	 */
 
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
@@ -73,6 +94,21 @@ namespace ft {
 		throw ft::invalid_argument();
 	}
 
+	/** @fn trueDistance
+   	  *
+   	  * @brief Fonction interne, permettant de gérer la distance selon le type d'iterator
+   	  *
+   	  * Fonction interne, permettant de gérer la distance selon le type d'iterator,
+   	  * ici la spécialisation pour les non random_access_iterator et les non output_iterator, utilisant la fonction iterDistance
+   	  *
+   	  * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+   	  * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+   	  * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+   	  * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+   	  * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+	  *
+  	  */
+
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
 	trueDistance(InputIterator first, InputIterator last, std::forward_iterator_tag tag) {
@@ -80,12 +116,42 @@ namespace ft {
 		return iterDistance(first, last);
 	}
 
+	/** @fn trueDistance
+  	 *
+  	 * @brief Fonction interne, permettant de gérer la distance selon le type d'iterator
+  	 *
+  	 * Fonction interne, permettant de gérer la distance selon le type d'iterator,
+  	 * ici la spécialisation pour les non random_access_iterator et les non output_iterator, utilisant la fonction iterDistance
+  	 *
+  	 * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+  	 * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+  	 * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+  	 * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+  	 * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+  	 *
+ 	 */
+
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
 	trueDistance(InputIterator first, InputIterator last, std::bidirectional_iterator_tag tag) {
 		(void) tag;
 		return iterDistance(first, last);
 	}
+
+	/** @fn trueDistance
+ 	  *
+ 	  * @brief Fonction interne, permettant de gérer la distance selon le type d'iterator
+ 	  *
+ 	  * Fonction interne, permettant de gérer la distance selon le type d'iterator,
+ 	  * ici la spécialisation pour les random_access_iterator, utilisant operator-
+ 	  *
+ 	  * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+ 	  * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+ 	  * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+ 	  * @param tag < Paramètre vide ne servant qu'a différencier les plusieurs prototypes de la fonction
+ 	  * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+ 	  *
+ 	*/
 
 	template<class InputIterator>
 	static typename iterator_traits<InputIterator>::difference_type
@@ -95,6 +161,19 @@ namespace ft {
 			return first - last;
 		return (last - first);
 	}
+
+	/** @fn distance
+	 *
+	 * @brief https://www.cplusplus.com/reference/iterator/distance/
+	 *
+	 * Donne la distance entre deux itérateurs
+	 *
+	 * @tparam InputIterator < Le type des deux paramètres, doit être un iterator
+	 * @param first < Un des deux iterator a comparer, usuellement "avant" le second
+	 * @param last  < Un des deux iterator a comparer, usuellement "après" le premier
+	 * @return Retourne la distance entre first et last, sous le type membre "difference_type" des iterators fournis
+	 *
+	 */
 
 	template<class InputIterator>
 	typename iterator_traits<InputIterator>::difference_type
