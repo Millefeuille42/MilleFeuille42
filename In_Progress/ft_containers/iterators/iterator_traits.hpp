5bfa94a3869db735ebe191de72a72da2bec51f6a
Millefeuille42 <mathieu.labourier@gmail.com>
2022-03-30 14:25:27 +0200

    Finished iterator_traits.hpp documentation.

--- In_Progress/ft_containers/iterators/iterator_traits.hpp
+++ In_Progress/ft_containers/iterators/iterator_traits.hpp
@@ -8,44 +8,58 @@
 #include "../utils/stddef.hpp"
 #include "../utils/type_traits.hpp"
 
-
-
-/// Defines the iterator_traits class
 namespace ft {
+	/** @struct iterator_traits
+	 * @brief https://www.cplusplus.com/reference/iterator/iterator_traits
+	 *
+	 * Une structure permettant de récupérer les différents membre types des iterator
+	 *
+	 * @tparam _iterator < L'iterator dont on veut récupérer les membres types
+	 */
 
 	template<typename _iterator>
 	struct iterator_traits {
-		typedef typename _iterator::iterator_category iterator_category;
-		typedef typename _iterator::value_type        value_type;
-		typedef typename _iterator::difference_type   difference_type;
-		typedef typename _iterator::pointer           pointer;
-		typedef typename _iterator::reference         reference;
+		typedef typename _iterator::iterator_category iterator_category;	/// < @brief Catégorie de l'iterator, usuellement un des iterator_tags
+		typedef typename _iterator::value_type        value_type;			/// < @brief Type sur lequel l'iterator pointe
+		typedef typename _iterator::difference_type   difference_type;		/// < @brief Type entier utilisé pour les operations arithmétiques
+		typedef typename _iterator::pointer           pointer;				/// < @brief Usuellement un pointeur sur type
+		typedef typename _iterator::reference         reference;			/// < @brief Usuellement une reference sur type
 	};
+
+	/** @struct iterator_traits
+ 	 * @brief https://www.cplusplus.com/reference/iterator/iterator_traits
+ 	 *
+ 	 * Une structure permettant de récupérer les différents membre types des iterator.
+ 	 * Ici, une spécialisation de template pour les pointeurs
+ 	 *
+ 	 * @tparam _t < Un pointeur passé en paramètre
+ 	 */
 
 	template<typename _t>
 	struct iterator_traits<_t*> {
-		typedef std::random_access_iterator_tag iterator_category;
-		typedef _t                         value_type;
-		typedef ptrdiff_t                   difference_type;
-		typedef _t*                        pointer;
-		typedef _t&                        reference;
+		typedef std::random_access_iterator_tag iterator_category;	/// < @brief Catégorie de l'iterator, un pointeur est un random_access_iterator
+		typedef _t                         value_type;				/// < @brief Type du pointeur
+		typedef ptrdiff_t                   difference_type;		/// < @brief Type entier utilisé pour les operations arithmétiques, par défaut ptrdiff_t
+		typedef _t*                        pointer;					/// < @brief Pointeur sur type
+		typedef _t&                        reference;				/// < @brief Référence sur type
 	};
+
+	/** @struct iterator_traits
+  	 * @brief https://www.cplusplus.com/reference/iterator/iterator_traits
+  	 *
+  	 * Une structure permettant de récupérer les différents membre types des iterator.
+  	 * Ici, une spécialisation de template pour les pointeurs sur const type
+  	 *
+  	 * @tparam _t < Un pointeur sur const type passé en paramètre
+  	 */
 
 	template<typename _t>
 	struct iterator_traits<const _t*> {
-		typedef std::random_access_iterator_tag iterator_category;
-		typedef _t                         value_type;
-		typedef ptrdiff_t                   difference_type;
-		typedef const _t*                  pointer;
-		typedef const _t&                  reference;
-	};
-
-	template<> struct iterator_traits<int> {
-		typedef std::random_access_iterator_tag iterator_category;
-		typedef int                         value_type;
-		typedef ptrdiff_t                   difference_type;
-		typedef int*                        pointer;
-		typedef int&                        reference;
+		typedef std::random_access_iterator_tag iterator_category;	/// < @brief Catégorie de l'iterator, un pointeur est un random_access_iterator
+		typedef const _t                         value_type;		/// < @brief Type const du pointeur
+		typedef ptrdiff_t                   difference_type;		/// < @brief Type entier utilisé pour les operations arithmétiques, par défaut ptrdiff_t
+		typedef const _t*                  pointer;					/// < @brief Pointeur sur const type
+		typedef const _t&                  reference;				/// < @brief Référence sur const type
 	};
 }
 
